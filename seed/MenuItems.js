2ea3594ba2422b3c628a622e0ce4715b7c82dfb1
vithya27 <108888457+vithya27@users.noreply.github.com>
2022-10-25 00:50:20 +0800

    MenuItems seed

---
+++ seed/MenuItems.js
@@ -0,0 +1,411 @@
+module.exports = [
+  {
+    name: "Egg Avocado Toast",
+    "price ": 13.9,
+    img: "https://imgur.com/1uIJ4vl",
+    description:
+      "Freshly sliced avocados atop toasty multigrain bread with drizzle of honey and a hint of lime, topped with fried eggs",
+    category: "All Day Breakfast",
+    tags: "Classic",
+  },
+  {
+    name: "Bacon Ham-Egg Sandwich",
+    "price ": 13.9,
+    img: "https://imgur.com/JqZgxQg",
+    description:
+      "Pan-fried bacon with chilled ham and freshly poached eggs sandwiched with sourdough slices",
+    category: "All Day Breakfast",
+  },
+  {
+    name: "BLT Sandwich",
+    "price ": 13.9,
+    img: "https://imgur.com/EWHLpgq",
+    description:
+      "Your standard bacon, lettuce and tomato combination, served with toasted sourdough slices",
+    category: "All Day Breakfast",
+  },
+  {
+    name: "Eggs Benedict (Ham or Bacon)",
+    "price ": 14.9,
+    img: "https://imgur.com/EEZwTGj",
+    description:
+      "Poached eggs topped on toast with a choice of bacon or ham on top",
+    category: "All Day Breakfast",
+  },
+  {
+    name: "2Six Breakfast",
+    "price ": 16.9,
+    img: "https://imgur.com/8HX7RxK",
+    description:
+      "Scrambled eggs, bacon, ham, sausage, baked beans, seared tomato and toast",
+    category: "All Day Breakfast",
+    tags: "Chef Recommendation",
+  },
+  {
+    name: "Ultimate Eggs Benedict",
+    "price ": 17.9,
+    img: "https://imgur.com/KtZXggc",
+    description:
+      "Poached eggs topped with hollandaise, avocado, beef patty, bacon, ham, toast and salad. ",
+    category: "All Day Breakfast",
+    tags: "Chef Recommendation",
+  },
+  {
+    name: "Bolognese Pasta (Spaghetti or Fusilli)",
+    "price ": 14.9,
+    img: "https://imgur.com/EcwAcKB",
+    description:
+      "Pasta tossed in homemade beef and tomato sauce. An Italian classic. ",
+    category: "Mains",
+    tags: "Classic",
+  },
+  {
+    name: "Carbonara Pasta (Spaghetti or Fusilli)",
+    "price ": 14.9,
+    img: "https://imgur.com/5nKlPDt",
+    description: "Pasta tossed in egg yolk and cream sauce topped with bacon. ",
+    category: "Mains",
+  },
+  {
+    name: "BBQ Jumbo Beef Burger",
+    "price ": 14.9,
+    img: "https://imgur.com/XfFHiPF",
+    description:
+      "Classic large American burger with beef patty, lettuce, tomatoes and cheese.",
+    category: "Mains",
+  },
+  {
+    name: "Bulgogi Burger",
+    "price ": 14.9,
+    img: "https://imgur.com/jBUeQCu",
+    description: "Japanese style beef patty in a classic burger.",
+    category: "Mains",
+  },
+  {
+    name: "Hawaiian Chicken Burger",
+    "price ": 14.9,
+    img: "https://imgur.com/3LoTqAG",
+    description:
+      "Burger with teriyaki marinated chicken, lettuce, tomatoes and mayonnaise sauce.",
+    category: "Mains",
+  },
+  {
+    name: "Teriyaki Chicken with Rice",
+    "price ": 14.9,
+    img: "https://imgur.com/mTVI7hp",
+    description: "Teriyaki chicken cutlet served with steamed jasmine rice.",
+    category: "Mains",
+  },
+  {
+    name: "Japanese Hamburger Rice",
+    "price ": 14.9,
+    img: "https://imgur.com/s6khGMl",
+    description: "Japanese style beef patty served with steamed jasmine rice. ",
+    category: "Mains",
+    tags: "Chef Recommendation",
+  },
+  {
+    name: "Japanese Curry Omu Rice with Chicken Cutlet",
+    "price ": 15.9,
+    img: "https://imgur.com/9soG5oH",
+    description:
+      "Japanese curry rice topped with scrambled eggs and a classic chicken cutlet.",
+    category: "Mains",
+    tags: "All time favourite",
+  },
+  {
+    name: "Gyudon",
+    "price ": 15.9,
+    img: "https://imgur.com/ZRYlN06",
+    description:
+      "Korean inspired marinated beef dish served with steamed jasmine rice. ",
+    category: "Mains",
+  },
+  {
+    name: "Kitsune Udon",
+    "price ": 9.9,
+    img: "https://imgur.com/sbMrHiU",
+    description:
+      "A vegetarian udon noodle soup topped with pieces of fried tofu.",
+    category: "Mains",
+  },
+  {
+    name: "Butter Lemon Baked Salmon",
+    "price ": 17.9,
+    img: "https://imgur.com/rGhZDQx",
+    description:
+      "Baked salmon seasoned with butter and lemon, served with steamed rice. ",
+    category: "Mains",
+  },
+  {
+    name: "Power Up! Chicken",
+    "price ": 13.9,
+    img: "https://imgur.com/HPOojQv",
+    description:
+      "Sous vide skinless chicken topped with hollandaise sauce, avocado, mashed potatoes and cherry tomatoes",
+    category: "Mains",
+  },
+  {
+    name: "Beef Lasagne",
+    "price ": 13.9,
+    img: "https://imgur.com/jDhnECD",
+    description:
+      "A classic Italian dish comprising sheets of pasta, cheese and minced beef, tossed in bolognese sauce.",
+    category: "Mains",
+  },
+  {
+    name: "Chicken Baked Rice",
+    "price ": 13.9,
+    img: "https://imgur.com/yRASi9x",
+    description: "Baked cheese rice topped with chicken",
+    category: "Mains",
+  },
+  {
+    name: "Mushroom Macaroni",
+    "price ": 13.9,
+    img: "https://imgur.com/382WRET",
+    description: "Mushroom in macaroni and cheese",
+    category: "Mains",
+  },
+  {
+    name: "American Beef Pie",
+    "price ": 6.9,
+    img: "https://imgur.com/HHTVbAM",
+    description: "American inspired minced beef in puff pastry",
+    category: "Pastries",
+    tags: "Chef's Recommendation",
+  },
+  {
+    name: "Chicken and Mushroom Pie",
+    "price ": 6.9,
+    img: "https://imgur.com/i3sKTqB",
+    description: "Chicken and mushrooms wrapped in puff pastry",
+    category: "Pastries",
+  },
+  {
+    name: "Caesar Salad",
+    "price ": 9.9,
+    img: "https://imgur.com/LPGA4f6",
+    description: "Mixed fresh salad greens with classic caesar sauce",
+    category: "Sides",
+    tags: "Healthy choice",
+  },
+  {
+    name: "Avocado Tofu Salad",
+    "price ": 10.9,
+    img: "https://imgur.com/9OFsNvo",
+    description:
+      "A healthy blend of avocado and tofu with mixed fresh salad greens",
+    category: "All Day Breakfast",
+    tags: "Healthy choice",
+  },
+  {
+    name: "Muffin (Banana Walnut/Chocolate/Blueberry)",
+    "price ": 4.2,
+    img: "https://imgur.com/vrYxhGg",
+    description: "Muffin (Banana Walnut/Chocolate/Blueberry)",
+    category: "Sides",
+  },
+  {
+    name: "Nachos",
+    "price ": 7.5,
+    img: "https://imgur.com/Zs5ij0h",
+    description: "Cheese and Mexican tortilla chips",
+    category: "Sides",
+  },
+  {
+    name: "Beef Slider",
+    "price ": 13.9,
+    img: "https://imgur.com/5N4Sjwg",
+    description: "Classic beef club sandwich",
+    category: "Sides",
+  },
+  {
+    name: "Chicken Slider",
+    "price ": 13.9,
+    img: "https://imgur.com/PLuzndb",
+    description: "Classic chicken club sandwich",
+    category: "Sides",
+  },
+  {
+    name: "Classic English Breakfast Tea",
+    "price ": 6.9,
+    img: "https://imgur.com/EHEqUUx",
+    description: "Pure ceylon black tea",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Eternal Garden Tea",
+    "price ": 6.9,
+    img: "https://imgur.com/MMjgS7D",
+    description: "Pure ceylon black tea, rose petals and rose flavour ",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Timeless Earl Grey",
+    "price ": 6.9,
+    img: "https://imgur.com/CyhOs1B",
+    description:
+      "Pure ceylon black tea with blue corn flower oil of bergamot and cream flavour ",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Fruitopia",
+    "price ": 7.9,
+    img: "https://imgur.com/Pzg72tC",
+    description:
+      "Pure ceylon black tea with a blend of fruit dices (apple, passion fruit, rapsberry, mango, soursop, orange and pineapple) and safflower",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Finest Darjeeling",
+    "price ": 8.9,
+    img: "https://imgur.com/4NdwGKd",
+    description:
+      "A classic Darjeeling Margaret's Hope with a beautiful muscatel flavour and aroma",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Black Tea Cocoa",
+    "price ": 8.9,
+    img: "https://imgur.com/Q6RfFuS",
+    description:
+      "A perfectly matched blend of black tea and cocoa, providing a unique flavour experience ",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Ever-Green Tea",
+    "price ": 6.9,
+    img: "https://imgur.com/Lvfv32j",
+    description:
+      "Ceylon hyson green tea from Sri Lanka's finest highland tea garden",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Lychee Fiesta",
+    "price ": 7.9,
+    img: "https://imgur.com/cx5n55F",
+    description: "Chinese green tea with rose petals and lychee flavour",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Genmaicha",
+    "price ": 7.9,
+    img: "https://imgur.com/jIRjAkm",
+    description:
+      "Second flush tea leaves of Hamada tea. It gives a bright green colour with an aroma of roasted brown rice grains.",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Gyokuro",
+    "price ": 8.9,
+    img: "https://imgur.com/3yXT1og",
+    description: "Shaded Japanese green tea from Yame. Fukuoka Prefecture.",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Chamomile",
+    "price ": 6.9,
+    img: "https://imgur.com/UKPoLfm",
+    description: "Roman chamomile whole flowers from the Nile River, Egypt",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Ginger",
+    "price ": 6.9,
+    img: "https://imgur.com/sLbsPX1",
+    description: "Dry ginger roots from Sri Lanka",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Lemongrass",
+    "price ": 6.9,
+    img: "https://imgur.com/XRHxAkH",
+    description: "Pure lemongrass from Thailand",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Peppermint",
+    "price ": 6.9,
+    img: "https://imgur.com/dJnvj8u",
+    description:
+      "Dry-cut peppermint leaves (native to the Mediterranean region from North America)",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Anti-Stress",
+    "price ": 7.9,
+    img: "https://imgur.com/tj06JYJ",
+    description:
+      "Rooibos, chamomile, honeybush, lemon balm fennel, lemongrass flavour and ginseng root",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Blissful Berries",
+    "price ": 7.9,
+    img: "https://imgur.com/i6JKAB4",
+    description:
+      "A blend of hibiscus petals, apple, elderberries, rosechips, raspberry, blackberry and strawberry ",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Healing Garden",
+    "price ": 7.9,
+    img: "https://imgur.com/lYYLzlA",
+    description:
+      "Ginger, lemongrass, lemon peel, ginseng root, spearmint and liquorice ",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Peace of Mind",
+    "price ": 7.9,
+    img: "https://imgur.com/n0nT6kc",
+    description:
+      "Leon balm, orange blossom, sweet blackberry leaves, cardamom hull, fennel and anise",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Kalahari Rooibos",
+    "price ": 8.9,
+    img: "https://imgur.com/afyxjWe",
+    description:
+      "Go on safari with this taste intensive Rooibos blend with lemongrass and hibiscus flowers",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Alisan Oolong",
+    "price ": 8.9,
+    img: "https://imgur.com/SdaW1Op",
+    description:
+      "Lightly-oxidated Oolong tea from Taiwan's world-famous Alishan Mountains",
+    category: "Beverages",
+    tags: "Tea",
+  },
+  {
+    name: "Capuccino",
+    "price ": 8.9,
+    img: "https://imgur.com/DFgPToq",
+    description: "Capuccino",
+    category: "Beverages",
+    tags: "Coffee",
+  },
+];
