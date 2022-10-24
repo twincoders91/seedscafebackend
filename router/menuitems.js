b461c0157a0be09322031a75f6af02a800cac523
vithya27 <108888457+vithya27@users.noreply.github.com>
2022-10-25 01:26:45 +0800

    Create menuitems.js

---
+++ router/menuitems.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const {
+  seed,
+  allMenuItems,
+  createMenuItem,
+  deleteMenuItem,
+  deleteAllMenuItems,
+  updateMenuItems,
+  findbyid,
+} = require("../controller/menuitems");
+
+//seed
+router.get("/seedmenuitems", seed);
+
+//view all menu items
+router.get("/allmenuitems", allMenuItems);
+
+//create new menu item
+router.put("/newmenuitem", createMenuItem);
+
+//delete
+router.delete("/deletemenuitem", deleteMenuItem);
+router.delete("/deleteallmenuitems", deleteAllMenuItems);
+
+//update
+router.patch("/updatemenuitem", updateMenuItems);
+
+//getbyid
+router.put("/findbyid", findbyid);
+
+module.exports = router;
